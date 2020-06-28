d3a6a9945fa6b2d1584c3a2631cce06921b72fce
okantumuklu <67555432+okantumuklu@users.noreply.github.com>
2020-06-28 20:44:28 +0300

    Update sifre.py

--- sifre.py
+++ sifre.py
@@ -1,6 +1,6 @@
 import random
 
-print (""" hello world""")
+print ("""Şifre oluşturma""")
 #alfabeler
 #ingilizce alfabe
 alfabeeng = ["a","b","c","d","e","f","g","h","ı","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
